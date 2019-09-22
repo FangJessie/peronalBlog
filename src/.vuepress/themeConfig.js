6fba9db0aff69ed2958be5be8b909463dcd9a590
Mr.Hope <zhangbowang1998@gmail.com>
2019-09-22 12:29:45 +0800

    添加全屏按钮 改进导航栏并添加导航栏图标 更改返回顶部按钮 完善vuepress配置 添加Github Pages域名 更新项目依赖

--- src/.vuepress/themeConfig.js
+++ src/.vuepress/themeConfig.js
@@ -2,12 +2,28 @@
  * @Author: Mr.Hope
  * @Date: 2019-09-16 22:17:34
  * @LastEditors: Mr.Hope
- * @LastEditTime: 2019-09-17 12:53:03
+ * @LastEditTime: 2019-09-22 12:28:05
  * @Description: 主题配置
  */
 
 /** 对默认主题的配置 */
 module.exports = {
+  /** 头部导航栏的配置 */
+  nav: [
+    { text: '主页', link: '/', icon: 'homefill' },
+    { text: '学习笔记', link: '/learning.html', icon: 'note' }
+  ],
+
+
+  /** 侧边栏配置 */
+  sidebar: {
+    // fallback
+    '/': [
+      '',      /* / */
+      'about'  /* /about.html */
+    ]
+  },
+
   /** 侧边栏标题显示深度，0-2 */
   sidebarDepth: 2,
 
@@ -15,60 +31,47 @@ module.exports = {
   locales: {
     /** 默认语言 */
     '/': {
-      /** 设置为中文 */
-      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
-
-      /** 网站在该语言下的标题 */
-      title: '我是方佳馨',
-
-      /** 网站在该语言下的描述 */
-      description: '方佳馨的个人博客',
-
-      /** 该语言下头部导航栏的配置 */
-      nav: [
-        { text: '主页', link: '/' },
-        { text: '学习笔记', link: '/learning.html' }
-      ],
-
-      /** 网站在该语言下的侧边栏 */
-      sidebar: {
-        // fallback
-        '/': [
-          '',      /* / */
-          'about'  /* /about.html */
-        ]
-      },
-
-      /** 多语言下拉菜单的标题 */
+      lang: 'zh-CN',
       selectText: '选择语言',
-
-      /** 该语言下的更新时间文字 */
       lastUpdated: '上次编辑时间',
-
-      /** 该语言在下拉菜单中的标签 */
-      label: '简体中文',
-
-      /** 编辑链接文字 */
-      editLinkText: '在 GitHub 上编辑此页'
+      label: '简体中文'
     },
 
     /** 英文 */
     '/en/': {
-      lang: 'en-US',
+      /** 设置该语言的代码 */
+      lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
+
+      /** 网站在该语言下的标题 */
       title: 'Jessie\'s Bolg',
+
+      /** 网站在该语言下的描述 */
       description: 'Vuepress based blog',
+
+      /** 该语言下头部导航栏的配置 */
       nav: [
-        { text: 'Home', link: '/en/' },
-        { text: 'Learning Note', link: '/en/learning.html' }
+        { text: 'Home', link: '/en/', icon: 'homefill' },
+        { text: 'Learning Note', link: '/en/learning.html', icon: 'note' }
       ],
+
+      /** 网站在该语言下的侧边栏 */
       sidebar: {
         '/en/': ['', 'about']
       },
+
+      /** 多语言下拉菜单的标题 */
       selectText: 'Language',
+
       /** 辅助标签 */
       ariaLabel: 'Select language',
+
+      /** 该语言下的更新时间文字 */
       lastUpdated: 'Last update',
+
+      /** 该语言在下拉菜单中的标签 */
       label: 'English',
+
+      /** 编辑链接文字 */
       editLinkText: 'Edit on Github'
     }
   },
